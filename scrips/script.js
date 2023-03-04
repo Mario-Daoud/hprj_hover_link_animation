d225a2737dca85f173f217315bf332ddf5e170f3
Mario Daoud <r0895582@student.vives.be>
2023-03-04 15:37:52 +0100

    added js
    variable css slider based on mouse posion of selected link

--- scrips/script.js
+++ scrips/script.js
@@ -1,4 +1,16 @@
 const setup = () => {
-    
+  for (const link of  document.getElementsByClassName("link")) {
+    link.onmousemove = (e) => {
+      const decimal = e.clientX / link.offsetWidth;
+
+      const basePercentage = 70,
+        percentageRange = 20,
+        adjustablePercent = percentageRange * decimal;
+
+        const colorPercent = basePercentage + adjustablePercent;
+        link.style.setProperty("--color-percent", `${colorPercent}%`);
+
+    }
+  }
 }
 window.addEventListener("load", setup);
